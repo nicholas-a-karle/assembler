4937218e81ba3a30cfbb19369cb90a9b92bb290a
Elrak01 <61857848+Elrak01@users.noreply.github.com>
2022-10-25 21:40:14 -0700

    Moved files into project, compiled and ran

--- src/App.java
+++ src/App.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class App {
     public static void main(String[] args) throws Exception {
         Assembler asm = new Assembler();
-        String p = "C:\\Users\\Nickk\\OneDrive\\Desktop\\nand2tetris\\projects\\06\\";
+        String p = "C:\\Users\\Nickk\\OneDrive\\Desktop\\nand2tetris\\projects\\06\\assembler\\src\\files\\";
         asm.assembleFile(p + "add\\Add.asm");
         asm.assembleFile(p + "max\\Max.asm");
         asm.assembleFile(p + "max\\MaxL.asm");
